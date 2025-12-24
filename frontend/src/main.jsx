1582d477c0c82df2944bd97d0c81493f7234d0b8
jeremiah-eth <ihejierechibueze@gmail.com>
2025-12-25 00:33:34 +0100

    ui: wallet-btn

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { StacksProvider } from './context/StacksContext';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <React.StrictMode>
+    <StacksProvider>
+      <App />
+    </StacksProvider>
+  </React.StrictMode>,
+);
